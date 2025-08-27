e52133b63516c47248592997083aa2d33b25319d
Srđan Vidović <vidosrd@gmail.com>
2025-08-27 12:15:10 +0200

    f

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,18 @@ class Post < ApplicationRecord
   has_rich_text :body
   has_one_attached :image
 
+  def self.order_by_votes(show = nil)
+    count = if show
+      "case when votes.show_id = #{show.id} AND NOT votes.fulfilled then 1 else null end"
+    else
+      nullif(votes.fulfilled, true)'
+    end
+    left_joins(:votes).group(:id)
+      .uniq!(:group)
+      .select("people.*, COUNT(#{count}) as vote_count")
+      .order(Arel.sql("COUNT(#{count}) DESC"))
+  end
+
   scope :search_by_name, ->(query) {
     where("titl ILIKE ?", "%#{query}%") }
   scope :search_by_name, ->(query) { where("name ILIKE ?", "%#{query}%") }
