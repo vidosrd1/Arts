8262fa348fc92dc24e2acf06a90b06eb102f40a7
Srđan Vidović <vidosrd@gmail.com>
2026-05-28 13:02:00 +0200

    1

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  root "home#index"
+  #root "home#index"
   # root "posts#index"
 end
