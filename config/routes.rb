a27538675f7337aabe3e20e692d7e1d521ce68cf
Srđan Vidović <vidosrd@gmail.com>
2026-06-10 20:12:50 +0200

    a

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
   get "home/index"
   get "home/in"
   get "home/google"
+  get '/pricing', to: 'home#pricing'
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
   get '/about', to: 'home#about'
   get "up" => "rails/health#show", as: :rails_health_check
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  #root "home#index"
+  root "home#index"
   # root "posts#index"
 end
